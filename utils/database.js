00929628769c80d2d97b1e2a14dc0628d71478ca
Joshtri <stuffofyos1516@gmail.com>
2024-01-29 06:44:03 +0800

    fix path logo

--- utils/database.js
+++ utils/database.js
@@ -1,22 +1,24 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool ({
     host:"banbqbykaghzzrsqrdei-mysql.services.clever-cloud.com", // local pc only.
     user: "uhwc7dnlccad96jy", //local pc .
     password:"uvznVcwfcyZSJ828Fb7t",
     database: "banbqbykaghzzrsqrdei",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect(function(err,results){
-
-    if(err){
-        console.log(`error connecting ${err.message}`);
-        
-    }
-    else if(!err){
-        // console.log(results);
-        console.log('connection database work :)');
-    }
+// Listener untuk menangkap event error
+db.on('error', (err) => {
+    console.error(`Database error: ${err.message}`);
 });
 
-module.exports=db;
+// Listener untuk menangkap event connect
+db.on('connect', () => {
+    console.log('Connected to the database!');
+  });
+  
+module.exports = db.promise(); 
+// module.exports=db;
